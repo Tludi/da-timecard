6dc54b9b023657075e20a038e59802d448705c91
Tludi <tludi00@gmail.com>
2016-01-14 18:45:22 -0800

    added project model and linked to workday

---
+++ app/serializers/project_serializer.rb
@@ -0,0 +1,3 @@
+class ProjectSerializer < ActiveModel::Serializer
+  attributes :id, :name, :account_id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   root 'dashboards#index'
 
   resources :workdays
