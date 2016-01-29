b0b20f953bab146ed2e2b3e8b39c59a2d873a29c
Tludi <tludi00@gmail.com>
2016-01-28 17:05:14 -0800

    converted hour fractions from percentage to minutes in workday model

--- app/models/workday.rb
+++ app/models/workday.rb
@@ -7,22 +7,28 @@ class Workday < ActiveRecord::Base
   accepts_nested_attributes_for :timePunches
   
   def self.calculateWorkhours(workday)
-    @punches = [] 
+    # get current workday punches and push times to array
+    @punches = []
     workdayPunches = workday.timePunches
     workdayPunches.each do |p|
       @punches << p.created_at
     end
 
+    # group punches in pairs then push to array
     punchGroups = []
-    @punches.in_groups_of(2, Time.now) do |group|
+    @punches.in_groups_of(2, Time.now) do |group| # Time.now is used when punch count is odd
       punchGroups << group
     end
 
+    # Iterate throug groups of punch pairs to get time difference
+    # and add each groups difference for a total hours worked
     @workHours = 0
     punchGroups.each do |pg|
       @workHours += TimeDifference.between(pg[0], pg[1]).in_hours
     end
-
-    return @workHours
+    # convert the fractional of hours worked from hour percentage to minutes
+    workHourModulus = @workHours.modulo(1)*0.6.round(2)
+    workHourWholeNumber = @workHours.round(0)
+    return workHourWholeNumber + workHourModulus
   end
 end
