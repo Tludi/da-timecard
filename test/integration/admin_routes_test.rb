bc28a00623be361824c1e17a5a9c23786671be1e
Tludi <tludi00@gmail.com>
2016-02-17 13:25:51 -0800

    added admin route test file

---
+++ test/integration/admin_routes_test.rb
@@ -0,0 +1,8 @@
+# admin_routes_test.rb
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionController::TestCase
+  test "Does this work?" do
+    get :index
+  end
+end
--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -5,4 +5,13 @@ class RoutesTest < ActionDispatch::IntegrationTest
     get "/"
     assert_response :success
   end
+
+  test "Root should be home#index" do
+    assert_routing '/', { controller: "home", action: "index"}
+  end
+
+  test "Root_path should be home#index" do
+    assert_routing root_path, { controller: "home", action: "index"}
+  end
+
 end
