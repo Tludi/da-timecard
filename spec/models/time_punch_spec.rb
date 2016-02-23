77ca4c636f742eb8be79ccc515e015a3e7509f79
Tludi <tludi00@gmail.com>
2016-02-23 13:28:16 -0800

    added explicit timePunch test

--- spec/models/time_punch_spec.rb
+++ spec/models/time_punch_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TimePunch, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with entry, clockedInStatus" do
+    timePunch = TimePunch.new(
+      entry: Time.now,
+      clockedInStatus: true
+    )
+    expect(timePunch).to be_valid
+  end
 end
