aea1d18639aa37ebc85a271cbe59d03871bbd9fe
Tludi <tludi00@gmail.com>
2016-02-23 14:37:27 -0800

    added test for alternating clockedInStatus for sequential timePunches

--- app/models/time_punch.rb
+++ app/models/time_punch.rb
@@ -1,3 +1,8 @@
 class TimePunch < ActiveRecord::Base
   belongs_to :workday
+
+  def invertStatus(currentStatus)
+    !currentStatus
+  end
+  
 end
--- spec/models/time_punch_spec.rb
+++ spec/models/time_punch_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe TimePunch, :type => :model do
     )
     expect(timePunch).to be_valid
   end
+
+  it "alternates clockedInStatus for sequential timePunches" do
+    initialStatus = false
+    initialTimePunch = create(:time_punch, clockedInStatus: !initialStatus)
+    currentStatus = initialTimePunch.clockedInStatus
+    # calls invertStatus method in timePunch model
+    # invertedCurrentStatus = initialTimePunch.invertStatus(currentStatus)
+    # secondTimePunch = build(:time_punch, clockedInStatus: invertedCurrentStatus)
+    secondTimePunch = build(:time_punch, clockedInStatus: !currentStatus)
+    expect(secondTimePunch.clockedInStatus).to eq false
+  end
 end
