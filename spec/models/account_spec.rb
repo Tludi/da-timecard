97f4c7b8ae3452d5dc615aa94017e47c83a23917
Tludi <tludi00@gmail.com>
2016-03-16 11:10:06 -0700

    cleaned up account spec test file

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Account, :type => :model do
-  it "is valid with a name, time zone" do
+RSpec.describe Account, type: :model do
+  it 'is valid with a name, time zone' do
     account = Account.new(
-      name: "diligent agility",
-      time_zone: "Eastern Time (US & Canada)"
+      name: 'diligent agility',
+      time_zone: 'Eastern Time (US & Canada)'
     )
     expect(account).to be_valid
   end
 
-  it "invalid without a name" do
+  it 'invalid without a name' do
     account = build(:invalid_account)
     account.valid?
     expect(account.errors[:name]).to include("can't be blank")
   end
 
-  it "invalid without a time_zone" do
+  it 'invalid without a time_zone' do
     account = build(:account, time_zone: nil)
     account.valid?
     expect(account.errors[:time_zone]).to include("can't be blank")
   end
-
 end
