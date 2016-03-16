e7a0c9c3c6b1d30e19b819966b9b59c6886248ca
Tludi <tludi00@gmail.com>
2016-03-16 11:47:10 -0700

    cleaned up application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
+# application_controller.rb
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :require_login
-  around_action :set_time_zone, if: :current_user
+  around_action :user_time_zone, if: :current_user
 
-
-  def is_admin?
-    current_user.role == "Admin"
+  def admin?
+    current_user.role == 'Admin'
   end
 
   private
-    def not_authenticated
-      redirect_to login_path, alert: "Please login first"
-    end
 
-    def set_time_zone(&block)
-      Time.use_zone(current_user.account.time_zone, &block)
-    end
+  def not_authenticated
+    redirect_to login_path, alert: 'Please login first'
+  end
 
+  def user_time_zone(&block)
+    Time.use_zone(current_user.account.time_zone, &block)
+  end
 end
