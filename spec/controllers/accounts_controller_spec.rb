154cb734d2b071a4f9864a1c8058793e084e77a7
Tludi <tludi00@gmail.com>
2016-03-16 11:42:21 -0700

    cleaned up accounts spec controller test

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Admin::AccountsController, :type => :controller do
-
+RSpec.describe Admin::AccountsController, type: :controller do
   before :each do
     @admin = create(:admin)
-    login_user(user = @admin, route = login_path)
+    login_user(@admin, login_path)
     @account = @admin.account
   end
 
-  describe "GET #index" do
-    it "renders the index template" do
+  describe 'GET #index' do
+    it 'renders the index template' do
       get :index
       expect(response).to render_template :index
     end
 
-    it "gets all of the accounts" do
+    it 'gets all of the accounts' do
       accounts = [@admin.account]
       accounts << create(:account)
       get :index
@@ -25,96 +24,92 @@ RSpec.describe Admin::AccountsController, :type => :controller do
     end
   end
 
-  describe "GET #show" do
-    it "renders the show template" do
+  describe 'GET #show' do
+    it 'renders the show template' do
       get :show, id: @account
       expect(response).to render_template :show
     end
 
-    it "assigns the requested account to @account" do
+    it 'assigns the requested account to @account' do
       get :show, id: @account
       expect(assigns(:account)).to eq @account
     end
   end
 
-  describe "GET #new" do
-    it "renders the new template" do
+  describe 'GET #new' do
+    it 'renders the new template' do
       get :new
       expect(response).to render_template :new
     end
 
-    it "assigns a new account to @account" do
+    it 'assigns a new account to @account' do
       get :new
       expect(assigns(:account)).to be_a_new(Account)
     end
   end
 
-  describe "GET #edit" do
-    it "renders the edit template" do
+  describe 'GET #edit' do
+    it 'renders the edit template' do
       get :edit, id: @account
       expect(response).to render_template :edit
     end
 
-    it "assigns the requested account to @account" do
+    it 'assigns the requested account to @account' do
       get :edit, id: @account
       expect(assigns(:account)).to eq @account
     end
   end
 
-  describe "POST #create" do
-    context "with valid attributes" do
-      it "saves the new account in the database" do
-        expect{ post :create, account: attributes_for(:account)}.to change(Account, :count).by(1)
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new account in the database' do
+        expect { post :create, account: attributes_for(:account) }.to change(Account, :count).by(1)
       end
 
-      it "creates a user with the role Admin on creation" do
-        expect{ post :create, account: attributes_for(:account)}.to change(User, :count).by(1)
+      it 'creates a user with the role Admin on creation' do
+        expect { post :create, account: attributes_for(:account) }.to change(User, :count).by(1)
       end
 
-      it "redirects to the admin_account#index page" do
+      it 'redirects to the admin_account#index page' do
         post :create, account: attributes_for(:account)
         expect(response).to redirect_to admin_accounts_path
       end
-
     end
 
-    context "with invalid attributes" do
-      it "does not save the new account in the database" do
-       expect{ post :create, account: attributes_for(:invalid_account)}.not_to change(Account, :count)
+    context 'with invalid attributes' do
+      it 'does not save the new account in the database' do
+        expect { post :create, account: attributes_for(:invalid_account) }.not_to change(Account, :count)
       end
 
-      it "re-renders the new template" do
+      it 're-renders the new template' do
         post :create, account: attributes_for(:invalid_account)
         expect(response).to render_template :new
       end
-
     end
-
   end
 
-  describe "PATCH #update" do
-    context "with valid attributes" do
-      it "locates the requested account in the database" do
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'locates the requested account in the database' do
         patch :update, id: @account, account: attributes_for(:account)
         expect(assigns(:account)).to eq @account
       end
 
-      it "changes accounts attributes" do
+      it 'changes accounts attributes' do
         patch :update, id: @account, account: attributes_for(:account, name: 'Seahawks')
         @account.reload
         expect(@account.name).to eq 'Seahawks'
       end
 
-      it "redirects to the account index when successful update" do
+      it 'redirects to the account index when successful update' do
         patch :update, id: @account, account: attributes_for(:account, name: 'Seahawks')
         @account.reload
         expect(response).to redirect_to admin_accounts_path
       end
-
     end
 
-    context "with invalid attributes" do
-      it "does not update the account" do
+    context 'with invalid attributes' do
+      it 'does not update the account' do
         patch :update, id: @account, account: attributes_for(:account,
                                                              name: 'Seahawks',
                                                              time_zone: nil)
@@ -122,21 +117,20 @@ RSpec.describe Admin::AccountsController, :type => :controller do
         expect(@account.name).to_not eq 'Seahawks'
       end
 
-      it "re-renders the edit template" do
+      it 're-renders the edit template' do
         patch :update, id: @account, account: attributes_for(:invalid_account)
         @account.reload
         expect(response).to render_template :edit
       end
     end
-
   end
 
-  describe "when DELETE #destroy" do
-    it "deletes the account from the database" do
-      expect{ delete :destroy, id: @account}.to change(Account, :count).by(-1)
+  describe 'when DELETE #destroy' do
+    it 'deletes the account from the database' do
+      expect { delete :destroy, id: @account }.to change(Account, :count).by(-1)
     end
 
-    it "redirects to the account#index" do
+    it 'redirects to the account#index' do
       delete :destroy, id: @account
       expect(response).to redirect_to admin_accounts_path
     end
