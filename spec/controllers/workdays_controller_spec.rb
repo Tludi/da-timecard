7df86baa8a8e2036724014a7e39b69804d6fde23
Tludi <tludi00@gmail.com>
2017-04-13 11:54:34 -0700

    commit before upgrading to rails 5

---
+++ spec/controllers/workdays_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe WorkdaysController, :type => :controller do
+  describe 'GET #new' do
+    it 'renders the new template'
+
+    it 'assigns an workday to @workday'
+  end
+
+  describe 'Get #show' do
+    it 'renders the show template' do
+      get :show
+      
+    end
+  end
+end
