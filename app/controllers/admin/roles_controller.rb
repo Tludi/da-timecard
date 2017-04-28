1ea0c02bfb966af22e2192d7f127098819a766be
Tludi <tludi00@gmail.com>
2017-04-28 16:17:09 -0700

    created controller and views for creating and editing user roles

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,16 +1,17 @@
 class Admin::RolesController < Admin::AdminController
+  before_action :set_role, only: [:edit, :update, :destroy]
+  before_action :set_account
 
   def index
-    @roles = userAccount.roles.all
+    @roles = @account.roles.all
   end
 
   def new
-    @role = userAccount.roles.new
+    @role = @account.roles.new
   end
 
   def create
-    @role = userAccount.roles.new(user_params)
-
+    @role = @account.roles.new(user_params)
 
     respond_to do |format|
       if @role.save
@@ -23,12 +24,43 @@ class Admin::RolesController < Admin::AdminController
     end
   end
 
+  def edit
+  end
+
   def update
+      respond_to do |format|
+      if @role.update(user_params)
+        format.html { redirect_to admin_roles_path, notice: 'Role was successfully updated.' }
+        format.json { render :index, status: :ok, location: @role }
+      else
+        format.html { render :edit }
+        format.json { render json: @role.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @role.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_roles_url, notice: 'Role was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_role
+      @role = Role.find(params[:id])
+    end
+
+    def set_account
+      @account = current_user.account
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:role).permit(:name, :account_id)
+    end
 
 
 end
